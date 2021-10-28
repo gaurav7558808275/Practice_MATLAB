fb04a8481729c39a3353b4ee240b742f3db4ee50
gaurav phadke <grvgaurav03@gmail.com>
2021-10-28 17:48:40 +0530

    Matrix manipulation

---
+++ logic.m
@@ -0,0 +1,7 @@
+%logic
+a=5;
+b=6;
+x = a<=b;
+y= a == b;
+disp(x);
+disp(y);
---
+++ matrix_mani.m
@@ -0,0 +1,10 @@
+%matrix manipulation
+clc;
+clear;
+close all;
+
+a= [1 2 3 ; 3 4 5 ; 5 7 6];
+disp(a);
+ 
+a(:,1)=[1 2 3]; % play around for row and column manipulation
+disp(a);
